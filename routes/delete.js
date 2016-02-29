a0351e5f48b536965584ee504f8d53cf529ccac0
graywind <xaj3ichi-gh@badadmin.net>
2016-02-28 16:24:13 -0800

    Proper response to delete now with refresh: true, needs cleanup

--- routes/delete.js
+++ routes/delete.js
@@ -5,19 +5,26 @@ var client = new elasticsearch.Client({
   log: 'trace'
 });
 
+
+
 exports.removeItem = function(req, res){
+	function myawesome() {
+		console.log ("I finished!");
+		res.redirect('/items');
+	}
     if (typeof req.session.username == 'undefined') res.redirect('/');
     else {
 	console.log(req.body);
 	client.delete({
 		index: 'ninjastore',
 		type: 'ninjatype',
-		id: req.body._method
-	}).then(function() {
+		id: req.body._method,
+		refresh: true
+	},myawesome)/*.done(function(error, response) {
 		//res.render('items', { title: 'Ninja Store - Items', username: req.session.username, items:items })
 		//TODO: AJAX call instead of lazy redirect
 		res.redirect('/items')
-	});
+	});*/
 
 	//TODO: AJAX call instead of lazy redirect
 	//res.redirect('/items')
